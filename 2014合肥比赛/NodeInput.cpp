dc903123e09fdbef589cd3034e46853ee969f758
mianbaoji <652193837@qq.com>
2015-02-08 21:43:11 +0800

    k

--- 2014合肥比赛/NodeInput.cpp
+++ 2014合肥比赛/NodeInput.cpp
@@ -1,8 +1,14 @@
 
 #include "NodeInput.h"
 
-NodeInput::NodeInput(CToAgent* m_psendToAgent, uagent* m_thePlayersInformation, 
-		uagent* m_tempThePlayersInformation)
+NodeInput::NodeInput(CToAgent** m_psendToAgent,
+	uagent* m_thePlayersInformation,
+	uagent* m_tempThePlayersInformation)
 {
-	for (int i = 0;i<)
+	for (int i = 0; i < PLAYER_NUMBER; i++)
+	{
+		m_pSendToAgent[i] = m_psendToAgent[i];
+		m_ThePlayersInformation[i] = &(m_thePlayersInformation[i]);
+		m_TempThePlayersInformation[i] = &(m_tempThePlayersInformation[i]);
+	}
 }
